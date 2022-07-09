7f994cbe440641397bcf9cbaa476a073a74e08a5
Suryash <joblesscoder2468@gmail.com>
2022-07-09 23:10:44 +0530

    Completed implementation of graph

--- Graph/implementation.cpp
+++ Graph/implementation.cpp
@@ -4,8 +4,51 @@ using namespace std;
 class graph{ 
 unordered_map<int, vector<int>> adj;
 
+public:
+void addEdges(int u, int v, bool direction){
+    adj[u].push_back(v);
+    if(!direction){
+        adj[v].push_back(u);
+    }
+    
+}
+void printGraph(){
+    for(auto it: adj){
+        cout<<it.first<<"->";
+        for(auto i: it.second){
+            cout<<i<<",";
+
+        }
+        cout<<endl;
+    }
+}
 
 }; 
-int main() {
+int main(){
+
+
+//Code with user input
+int n, m;
+cout<<"Number of nodes:";
+cin>>n;
+cout<<"Number of edges:";
+cin>>m;
+graph G;
+for(int i=0; i<m; i++){
+    int u, v;
+    bool direction;
+    cin>>u>>v>>direction;
+    G.addEdges(u, v, direction);
+
+}
+G.printGraph();
+return 0;
+//Basic Code with no user-input
+// graph G;
+// G.addEdges(0, 1, 0);
+// G.addEdges(1, 2, 0);
+// G.addEdges(2, 3, 0);
+// // G.addEdges(3, 4, 0);
+
 
 }
